efa7e0d0cde6d365abd5e8eef1d42bffb69be95d
chenjie <chenjie@uino.com>
2022-11-28 10:56:04 +0800

    更新

--- mongodb/mongodb-cluster-single/scripts/init-auth.sh
+++ mongodb/mongodb-cluster-single/scripts/init-auth.sh
@@ -5,7 +5,7 @@ db.createUser({
   user: "root",
   pwd: "root123456",
   roles: [
-    { role: 'userAdminAnyDatabase', db: 'admin' }
+    { role: 'root', db: 'admin' }
   ]
 })
 db = db.getSiblingDB('cqjc')
